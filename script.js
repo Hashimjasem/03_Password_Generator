f28f6617274e6f162144fc8ee7b72bdf8a632495
hashim <j_hashim1305@outlook.com>
2022-08-31 20:56:07 +0800

    planning

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
-
+//this is the function that will run once the event is triggered
 function generatePassword() {
 
-  //....
+
+  // ask how many caracters the password should be
+
+  //inclue the following parameteres;
+  //uppercase
+  //lowercase
+  //numbers
+  //special symbols
+
+  //generate password based on criterias selected and length
+  
+  //1. create the character set based on criterias
+  //2. loop for 'length' times, each iteration grab a random char and append to an accumulator
+  //3. 
+
+  //bank of characters
+  //abcde....ABCDE....1234....
+
+  //length
+  //loop
+
+
+  //password is a set off random characters
+
+
+
+  // return the password
 
 }
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
 
@@ -17,4 +43,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// when we click on generateBtn, then we will execute writePassword
 generateBtn.addEventListener("click", writePassword);
