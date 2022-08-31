a31003a8de77c97a5235fd7c3a538aabd7e1202f
hashim <j_hashim1305@outlook.com>
2022-08-31 21:47:40 +0800

    askes user for the perameters of the password

--- script.js
+++ script.js
@@ -4,32 +4,58 @@ const generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
 
-  // ask how many caracters the password should be
+// ask how many caracters the password should be
+//convert into a number instead of string 
+const length = Number(prompt("how many Characters?"));
 
-  //inclue the following parameteres;
-  //uppercase
-  //lowercase
-  //numbers
-  //special symbols
-
-  //generate password based on criterias selected and length
-  
-  //1. create the character set based on criterias
-  //2. loop for 'length' times, each iteration grab a random char and append to an accumulator
-  //3. 
-
-  //bank of characters
-  //abcde....ABCDE....1234....
-
-  //length
-  //loop
-
-
-  //password is a set off random characters
+//if user types in random rubish, handle it
+//if left blank, handle it
+//if length is less than 8, handle it
 
 
 
-  // return the password
+
+//check for minimum lenth of 8
+if(length < 8){}
+
+
+
+
+
+//inclue the following parameteres;
+//uppercase
+const includeUppercase = confirm("do You want uppercase?");
+//lowercase
+const includeLowercase = confirm("do You want lowercase?");
+//numbers
+const includeNumbers = confirm("do You want numbers?");
+//special symbols
+const includeSymbols = confirm("do You want symbols?");
+
+
+
+
+
+
+
+//generate password based on criterias selected and length
+
+//1. create the character set based on criterias
+//2. loop for 'length' times, each iteration grab a random char and append to an accumulator
+//3. 
+
+//bank of characters
+//abcde....ABCDE....1234....
+
+//length
+//loop
+
+
+//password is a set off random characters
+
+
+
+// return the password
 
 }
 
